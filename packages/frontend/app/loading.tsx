8d6162b16d2bf39a2666ed34981830bf6d7d3ad4
Koushik <koushiksinha12@gmail.com>
2025-07-18 01:36:06 -0400

    feat(frontend): add loading spinner

--- packages/frontend/app/loading.tsx
+++ packages/frontend/app/loading.tsx
@@ -1,3 +1,17 @@
+"use client"
+import { Box, CircularProgress } from "@mui/material"
+
 export default function Loading() {
-  return null
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress color="primary" />
+    </Box>
+  )
 }
